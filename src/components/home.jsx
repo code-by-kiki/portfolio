84bc753fd49e8a639fb9e7fa7c1eb7b5c693d4c1
krithika <krithikaselvam68@gmail.com>
2025-09-25 08:03:26 +0530

    Remove unwanted files

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -23,7 +23,7 @@ const Introduction = () => {
   <div className="col-12 col-md-7 animate__animated animate__fadeInRight px-3">
     <h2 className="text-center fw-bold mb-4" style={{ color: '#715a5a', fontFamily: 'Playfair Display, serif' }}><i>Introduction</i> </h2>
     <div className="p-2 col-sm-12" >
-      <p className="fs-5  " style={{ textIndent: '1em', lineHeight: '1.5',textAlign:'justify',textIndent:'100px' }}>
+      <p className="fs-5  " style={{ textIndent: '1em', lineHeight: '1.5',textAlign:'justify' }}>
        Hi, I’m Krithika B.A.Economics graduate with a deep passion for web development and digital innovation. My academic background has sharpened
         my analytical thinking and decision-making abilities, allowing me to assess solutions from multiple angles and choose the most effective 
         path forward.As a web developer, I prioritize the user experience, crafting intuitive and responsive web pages that are both visually 
